ff229511abc4a6dc070e78e2263ba59bfcf73bb3
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2015-01-20 13:25:08 +0900

    railsアプリを追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGenomerefine::Application.config.secret_token = '0401851a376f03ecf80fc9f2a5aecc802d41738e7f43a043510205c16b990d39a8c2cefc97fdf9c6ac12b6a1d694998afc392169067daf2f5b1c36bc2e886a29'
